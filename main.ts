cf84b9794c3fe32a10f7fa6826a1e84e8de7258c
CHSstudent <65049798+CHSstudent@users.noreply.github.com>
2022-09-29 08:46:08 -0700

    make fruit appear from side of screen

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+let projectile: Sprite = null
 let mrMacD = sprites.create(assets.image`MrMacD`, SpriteKind.Player)
 scene.setBackgroundColor(9)
 scene.setBackgroundImage(assets.image`moonBase`)
 mrMacD.scale = 1
+controller.moveSprite(mrMacD)
+mrMacD.setStayInScreen(true)
+info.setScore(0)
+info.startCountdown(10)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(assets.image`cherry`, 50, 0)
+    projectile.setPosition(0, randint(0, 120))
+})
